4612f3f1849a6931cbf29563ddaff6c65ce21cfa
rehanashaik2427 <shaikrehana2427@gmail.com>
2024-04-29 12:15:52 +0530

    c

--- src/pages/Companies.js
+++ src/pages/Companies.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-
+import './Home.css';
 const Companies = () => {
   const [formData, setFormData] = useState({
     companyName: '',
